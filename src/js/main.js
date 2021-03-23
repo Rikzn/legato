14c9f4b5e72a6104290308576a7c831b7aa2ce1e
rishat <hrrkzn@yandex.ru>
2021-03-23 05:48:27 +0300

    f1

--- src/js/main.js
+++ src/js/main.js
@@ -23,3 +23,25 @@ window.addEventListener('load', function() {
     document.body.classList.add('loaded');
     setTimeout(() => document.body.classList.add('animatable'), 300)
 })
+
+slider = new juxtapose.JXSlider('#slider-tablecloth',
+    [
+        {
+            src: 'https://example.com/firstimage.jpg',
+            label: '2009',
+            credit: 'Image Credit'
+        },
+        {
+            src: 'https://example.com/secondimage.jpg',
+            label: '2014',
+            credit: "Image Credit"
+        }
+    ],
+    {
+        animate: true,
+        showLabels: true,
+        showCredits: true,
+        startingPosition: "50%",
+        makeResponsive: true,
+        width: 450
+    });
